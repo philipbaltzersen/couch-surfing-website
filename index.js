d816bf61ab254f222219e86d089cd70f62340edf
Philip Holm <philipbaltzersen@gmail.com>
2024-02-29 06:48:22 +0100

    Parse ints

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const secondInput = document.querySelector("#second-input")
 const screen = document.querySelector(".screen")
 
 function addNumbers(a, b) {
-    console.log(a + b)
-    screen.innerHTML = a + b
+    if (typeof a === "number" && typeof b === "number") {
+        screen.innerHTML = a + b
+    } else {
+        screen.innerHTML = parseInt(a) + parseInt(b)
+    }
 }
 
 button.addEventListener("click", () => addNumbers(firstInput.value, secondInput.value))
