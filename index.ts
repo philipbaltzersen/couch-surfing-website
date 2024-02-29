354d67562cb65c2739d9131147f9b5bd7e31812b
Philip Holm <philipbaltzersen@gmail.com>
2024-02-29 07:24:31 +0100

    Show total reviews

--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
-let age: number = 100
-let firstName: string = "Philip"
-let busy: boolean = true
+const reviewTotalDisplay = document.querySelector("#reviews")
+
+const reviews = [
+    {
+        name: "Sheia",
+        stars: 5,
+        loyaltyUser: true,
+        date: "01-04-2021"
+    },
+    {
+        name: "Andrzej",
+        stars: 3,
+        loyaltyUser: false,
+        date: "28-03-2021"
+    },
+    {
+        name: "Omar",
+        stars: 4,
+        loyaltyUser: true,
+        date: "27-03-2021"
+    },
+]
+
+
+function showReviewsTotal (value: number) {
+    reviewTotalDisplay.innerHTML = value.toString()
+}
+
+showReviewsTotal(reviews.length)
