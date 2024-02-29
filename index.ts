808e0f143d4c41059a71febcbaafd5b602dc9f25
Philip Holm <philipbaltzersen@gmail.com>
2024-02-29 06:54:33 +0100

    Remove calculator

--- index.ts
+++ index.ts
@@ -1,10 +1,3 @@
-const button = document.querySelector(".button")
-const firstInput = document.querySelector("#first-input")
-const secondInput = document.querySelector("#second-input")
-const screen = document.querySelector(".screen")
-
-function addNumbers(a, b) {
-    screen.innerHTML = a + b
-}
-
-button.addEventListener("click", () => addNumbers(firstInput.value, secondInput.value))
+let age = 100
+let firstName = "Philip"
+let busy = true
