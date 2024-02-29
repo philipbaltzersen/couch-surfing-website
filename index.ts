eedc0963d03fa7735344093043bfd688b1943457
Philip Holm <philipbaltzersen@gmail.com>
2024-02-29 06:50:25 +0100

    Change to typescript

--- index.ts
+++ index.ts
@@ -4,11 +4,7 @@ const secondInput = document.querySelector("#second-input")
 const screen = document.querySelector(".screen")
 
 function addNumbers(a, b) {
-    if (typeof a === "number" && typeof b === "number") {
-        screen.innerHTML = a + b
-    } else {
-        screen.innerHTML = parseInt(a) + parseInt(b)
-    }
+    screen.innerHTML = a + b
 }
 
 button.addEventListener("click", () => addNumbers(firstInput.value, secondInput.value))
